239f0e46a58a9c97bd8bc824eb53ebb11b93b5dc
Malcolm Leake <echelon1089@gmail.com>
2019-05-05 22:39:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,5 +26,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.book_id) AS num_books FROM character_books
+  INNER JOIN characters ON character_books."
 end
